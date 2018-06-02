2ec2dccbb0aa6633493f4a25e8292ef87c8d0897
Sonny Martin <sonny.martin@diffblue.com>
2018-06-02 15:09:37 +0100

    Initial ok.

---
+++ build_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "------------------------- compiling java"
+javac HelloWorld.java
+echo "------------------------- running java" 
+java HelloWorld
+echo "------------------------- running python" 
+python HelloWorld.py
+echo "------------------------- build_script has finished!"
